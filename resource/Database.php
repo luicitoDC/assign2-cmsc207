0177070f8bb53358f646f340c1de03dcc1b7b3e0
luicitoDC <62644322+luicitoDC@users.noreply.github.com>
2022-03-25 20:39:23 -0600

    Update Database.php

--- resource/Database.php
+++ resource/Database.php
@@ -1,8 +1,12 @@
 <?php
 //Initialize variables that will store the connection parameters
-$username = 'root';
-$dsn = 'mysql:host=localhost; dbname=register';
-$password = '';
+//$username = 'root';
+//$dsn = 'mysql:host=localhost; dbname=register';
+//$password = '';
+
+$username = 'sql3481440';
+$dsn = 'mysql:host=sql3.freemysqlhosting.net:3306; dbname=sql3481440';
+$password = 'baNB8g4Pe4';
 
 try{
     //Create an instance of the PDO class with the required parameters
